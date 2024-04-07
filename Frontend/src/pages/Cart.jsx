bbdba1c4a415b82a0a87c3c97292c89bbbddab1f
samrat222 <samratborah111@gmail.com>
2024-04-08 02:36:09 +0530

    Fix the navbar

--- Frontend/src/pages/Cart.jsx
+++ Frontend/src/pages/Cart.jsx
@@ -13,39 +13,42 @@ const Cart = () => {
   const totalAmount = getTotalCartAmount();
   const navigate = useNavigate();
   return (
-    <Container>
+    <>
       <Navbar />
-      <div className='cart'>
-        <h1>
-          Your cart Items
-        </h1>
-      </div>
-      <div className="cartItems" >
-        {popularProducts.map((product) => {
-          if (cartItems[product.id] !== 0) {
-            return <CartItem data={product} />;
-          }
-        })}
-      </div>
+      <Container>
 
-      {totalAmount > 0 ? (
-        <div className="checkout">
-          <p> Subtotal: ${totalAmount} </p>
-          <button onClick={() => navigate("/")}> Continue Shopping </button>
-          <button
-            onClick={() => {
-              checkout();
-              navigate("/checkout");
-            }}
-          >
-            {" "}
-            Checkout{" "}
-          </button>
+        <div className='cart'>
+          <h1>
+            Your cart Items
+          </h1>
         </div>
-      ) : (
-        <h1> Your Shopping Cart is Empty</h1>
-      )}
-    </Container>
+        <div className="cartItems" >
+          {popularProducts.map((product) => {
+            if (cartItems[product.id] !== 0) {
+              return <CartItem data={product} />;
+            }
+          })}
+        </div>
+
+        {totalAmount > 0 ? (
+          <div className="checkout">
+            <p> Subtotal: ${totalAmount} </p>
+            <button onClick={() => navigate("/")}> Continue Shopping </button>
+            <button
+              onClick={() => {
+                checkout();
+                navigate("/checkout");
+              }}
+            >
+              {" "}
+              Checkout{" "}
+            </button>
+          </div>
+        ) : (
+          <h1> Your Shopping Cart is Empty</h1>
+        )}
+      </Container>
+    </>
   )
 }
 
